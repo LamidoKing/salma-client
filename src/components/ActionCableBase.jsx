b773d942d2603d283bd8e5320a68b8bffb41044b
Salma <salmawasee9@gmail.com>
2021-04-10 08:06:26 +0300

    updated

--- src/components/ActionCableBase.jsx
+++ src/components/ActionCableBase.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 import userService from "../services/user-service";
 import actioncable from 'actioncable';
 
+const cableURL =
+  window.location.hostname === "localhost"
+    ? "ws://localhost:3000/cable"
+    : "wss://fast-ridge-64559.herokuapp.com/cable"
+
 class ActionCableBase extends React.Component {
   constructor() {
     super();
-    this.consumer = actioncable.createConsumer(`${process.env.REACT_APP_SOCKET_URL}/cable?token=${userService.token()}`)
+    this.consumer = actioncable.createConsumer(`${cableURL}/cable?token=${userService.token()}`)
   }
   
   render() { 
